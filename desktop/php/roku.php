2b0bd6345a1ede74efa6d85a711370acaacb8dac
sarakha63 <sarakha_ludovic@yahoo.fr>
2017-09-27 17:01:15 +0200

    update

--- desktop/php/roku.php
+++ desktop/php/roku.php
@@ -65,12 +65,23 @@ $eqLogics = eqLogic::byType('roku');
     </div>   
 
     <div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-        <div class="row">
-            <div class="col-sm-6">
-		<form class="form-horizontal">
+       <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+  <a class="btn btn-default eqLogicAction pull-right" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
+  <ul class="nav nav-tabs" role="tablist">
+    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
+    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+  </ul>
+  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+      <br/>
+      <div class="row">
+        <div class="col-sm-6">
+          <form class="form-horizontal">
             <fieldset>
-                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"id="refreshpage"></i> {{Général}}<i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-                <div class="form-group">
+              <div class="form-group">
                     <label class="col-sm-4 control-label">{{Nom du dispositif}}</label>
                     <div class="col-sm-3">
                         <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
@@ -91,12 +102,12 @@ $eqLogics = eqLogic::byType('roku');
                     </div>
                 </div>
                 <div class="form-group">
-                <label class="col-sm-4 control-label" ></label>
-                <div class="col-sm-5">
-                 <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-                  <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+                <label class="col-sm-4 control-label"></label>
+                <div class="col-lg-6">
+                  <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                  <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
                 </div>
-                </div>
+              </div>
                 <legend><i class="fa fa-wrench"></i>  {{Configuration}}</legend>
 				<div class="form-group">
 				<label class="col-sm-4 control-label">{{Modèle}}</label>
@@ -134,16 +145,11 @@ $eqLogics = eqLogic::byType('roku');
 			</div>
 		</div>
 		</div>
-		<form class="form-horizontal">
-            <fieldset>
-			    <div class="form-actions" align="right">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                </div>
-            </fieldset>
-        </form>
-        <legend><i class="fa fa-list-alt"></i>  {{Tableau de commandes}}</legend>
-        <table id="table_cmd" class="table table-bordered table-condensed">
+		</div>
+		
+        <div role="tabpanel" class="tab-pane" id="commandtab">
+  <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
+  <table id="table_cmd" class="table table-bordered table-condensed">
             <thead>
                 <tr>
                     <th style="width: 700px;">{{Nom}}</th><th>{{Action}}</th>
@@ -154,16 +160,11 @@ $eqLogics = eqLogic::byType('roku');
             </tbody>
         </table>
 
-        <form class="form-horizontal">
-            <fieldset>
-			    <div class="form-actions">
-                    <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                    <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                </div>
-            </fieldset>
-        </form>
+        
 
     </div>
+</div>
+</div>
 </div>
 
 <?php include_file('desktop', 'roku', 'js', 'roku'); ?>
